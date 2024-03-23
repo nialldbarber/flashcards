8b1ab10448667aede17f4fa7a4fb47a707cb0f4c
Niall Barber <niall.barber@olioex.com>
2024-03-23 14:22:20 +0000

    feat: begin codemagic

--- app/store/preferences.ts
+++ app/store/preferences.ts
@@ -1,19 +1,25 @@
 import { create } from "zustand";
 import { createJSONStorage, persist } from "zustand/middleware";
 
-import { storeMiddleware } from "app/utils/store-middleware";
+import { storeMiddleware } from "#/app/utils/store-middleware";
 
 type PreferencesState = {
 	hapticFeedback: boolean;
 	setHapticFeedback: (hapticFeedback: boolean) => void;
+	language: "en" | "lv";
+	setLanguage: (language: "en" | "lv") => void;
 };
 
 export const usePreferencesStore = create(
 	persist<PreferencesState>(
 		(set) => ({
 			hapticFeedback: true,
+			language: "en",
 			setHapticFeedback: (hapticFeedback: boolean) => {
 				set({ hapticFeedback });
+			},
+			setLanguage: (language: "en" | "lv") => {
+				set({ language });
 			},
 		}),
 		{
